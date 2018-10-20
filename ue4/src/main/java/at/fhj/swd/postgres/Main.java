e6a761ff06ca5569f2383cec89def94367f6b6b1
Michael Klug <michael.klug2@edu.fh-joanneum.at>
2018-10-20 13:18:37 +0200

    working one to one relation

--- ue4/src/main/java/at/fhj/swd/postgres/Main.java
+++ ue4/src/main/java/at/fhj/swd/postgres/Main.java
@@ -2,54 +2,15 @@
 
 package at.fhj.swd.postgres;
 
-import at.fhj.swd.postgres.ProductsEntity;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
-import static org.junit.Assert.assertNotNull;
-
 public class Main {
-//    private static final SessionFactory ourSessionFactory;
-//
-//    static {
-//        try {
-//            Configuration configuration = new Configuration();
-//            configuration.configure();
-//
-//            ourSessionFactory = configuration.buildSessionFactory();
-//        } catch (Throwable ex) {
-//            throw new ExceptionInInitializerError(ex);
-//        }
-//    }
-//
-//    public static Session getSession() throws HibernateException {
-//        return ourSessionFactory.openSession();
-//    }
 
-    //    public static void main(final String[] args) throws Exception {
     public static void main(String[] args) {
-//        final Session session = getSession();
-//        try {
-//            System.out.println("querying all the managed entities...");
-//            final Metamodel metamodel = session.getSessionFactory().getMetamodel();
-//            for (EntityType<?> entityType : metamodel.getEntities()) {
-//                final String entityName = entityType.getName();
-//                final Query query = session.createQuery("from " + entityName);
-//                System.out.println("executing: " + query.getQueryString());
-//                for (Object o : query.list()) {
-//                    System.out.println("  " + o);
-//                }
-//            }
-//        } finally {
-//            session.close();
-//            ourSessionFactory.close();
-//        }
 
-
-// SETUP
         EntityManagerFactory factory;
         EntityManager manager;
         EntityTransaction transaction;
@@ -57,25 +18,25 @@ public class Main {
         final String persistenceUnitName = "products";
 
         factory = Persistence.createEntityManagerFactory(persistenceUnitName);
-        assertNotNull(factory);
         manager = factory.createEntityManager();
-        assertNotNull(manager);
 
         transaction = manager.getTransaction();
 
-// DO SOMETHING
-        for (int i = 0; i <= 10; i++) {
+        transaction.begin();
+        CityEntity testCity = new CityEntity();
+        testCity.setId(1);
+        testCity.setName("Graz");
+        manager.persist(testCity);
+        transaction.commit();
 
-            transaction.begin();
-            ProductsEntity testProduct = new ProductsEntity("Product " + i, ProductBarcode.EAN128);
-//            testProduct.setId(i);
-//            testProduct.setName("Product" + i);
-            assertNotNull(testProduct);
-            manager.persist(testProduct);
-            transaction.commit();
+        transaction.begin();
+        SightEntity testSight = new SightEntity();
+        testSight.setId(1);
+        testSight.setName("Uhrturm");
+        testSight.setSightToCity(testCity);
+        manager.persist(testSight);
+        transaction.commit();
 
-            System.out.println("Created and Persisted " + testProduct);
-        }
 
 // TEARDOWN
         manager.close();
---
+++ ue4/src/main/java/at/fhj/swd/postgres/SightEntity.java
@@ -0,0 +1,61 @@
+package at.fhj.swd.postgres;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "sight", schema = "ue04", catalog = "swd_ws18_13")
+public class SightEntity {
+    private int id;
+    private String name;
+
+    @Id
+    @Column(name = "id", nullable = false)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "name", nullable = true, length = 30)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        SightEntity that = (SightEntity) o;
+
+        if (id != that.id) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
+    private CityEntity sightToCity;
+
+    @OneToOne
+    public CityEntity getSightToCity() {
+        return sightToCity;
+    }
+
+    public void setSightToCity(CityEntity sightToCity) {
+        this.sightToCity = sightToCity;
+    }
+}
